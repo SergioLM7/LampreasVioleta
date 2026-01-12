eec05210b67a5e4c46ec85fca6cbdcf89c458c75
Sergio Lillo <sergiolillom@gmail.com>
2026-01-12 19:51:41 +0100

    ClienteDetalle Service to synchronize Cliente and ClienteDetalle save and rollback

--- src/main/java/services/ClienteDetalle.java
+++ src/main/java/services/ClienteDetalle.java
@@ -2,27 +2,45 @@ package services;
 
 import dao.ClienteDAO;
 import dao.DetalleClienteDAO;
+import db.Db;
 import model.Cliente;
 import model.DetalleCliente;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class ClienteDetalle {
 
-    private final ClienteDAO clienteDAO= new ClienteDAO();
-    private final DetalleClienteDAO detalleClienteDAO= new DetalleClienteDAO();
+    private final ClienteDAO clienteDAO = new ClienteDAO();
+    private final DetalleClienteDAO detalleClienteDAO = new DetalleClienteDAO();
 
-    public void guardarClienteCompleto(Cliente c, DetalleCliente d) {
+    /**
+        Función para gestionar el guardado del Cliente con sus DetalleCliente simultáneamente
+     **/
+    public void guardarClienteCompleto(Cliente c, DetalleCliente dc) throws SQLException {
+        try (Connection con = Db.getConnection()) {
 
-        try {
+            //Desactivar auto-commit del motor de BBDD
+            con.setAutoCommit(false);
 
+            try {
+                clienteDAO.insert(c, con);
+                detalleClienteDAO.insert(dc, con);
 
+                //Consolidamos las inserciones anteriores (por haber desactivado el auto-commit)
+                con.commit();
 
-
-        }catch () {
-
-        } finally {
-
+            } catch(SQLException e) {
+                con.rollback();
+                throw e;
+            } finally {
+                //Revertimos el cambio de la configuración de AutoCommit, para que se puedan guardar otras transacciones
+                //en otros puntos del código de nuestra aplicación
+                con.setAutoCommit(true);
+            }
         }
 
-
     }
+
+
 }
