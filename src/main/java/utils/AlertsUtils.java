60e83f891f57e8ad4ee82dc468338f68bf53eaf2
Sergio Lillo <sergiolillom@gmail.com>
2026-01-13 21:12:46 +0100

    Refactor Alerts in Utils

---
+++ src/main/java/utils/AlertsUtils.java
@@ -0,0 +1,31 @@
+package utils;
+
+import javafx.scene.control.Alert;
+
+public class AlertsUtils {
+
+    public static void mostrarError(String titulo, Exception e) {
+        e.printStackTrace();
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(titulo);
+        alert.setContentText(e.getMessage());
+        alert.showAndWait();
+    }
+
+    public static  void mostrarAlerta(String titulo, String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Aviso");
+        alert.setHeaderText(titulo);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
+    public static  void mostrarInfo(String titulo, String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Información");
+        alert.setHeaderText(titulo);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+}
